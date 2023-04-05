2651d2bbab388c9d7e7068db313fd97cd694695d
Genesis Doge <doge666@generative.xyz>
2023-04-05 15:41:27 +0700

    Add isOrdinal to quicknode API

---
+++ internal/usecase/structure/ordInscriptions.go
@@ -0,0 +1,18 @@
+package structure
+
+type InscriptionByOutput struct {
+	Outpoint     string   `json:"outpoint"`
+	Chain        string   `json:"chain"`
+	Inscriptions []string `json:"inscriptions"`
+}
+
+type WalletAddressBalanceResp struct {
+	Version  int `json:"version"`
+	Height   int64  `json:"height"`
+	Script   string `json:"script"`
+	Address  string  `json:"address"`
+	Coinbase bool   `json:"coinbase"`
+	Hash     string `json:"hash"`
+	Index    int    `json:"index"`
+	IsOrdinal bool  `json:"isOrdinal"`
+}
