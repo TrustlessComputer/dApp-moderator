9ea6bafe16832e9a63e3def0fce29523197c7d34
Phong Nguyen <phongnguyen@Phongs-MacBook-Pro.local>
2023-04-06 15:26:51 +0700

    add crawler for token

---
+++ internal/entity/token.go
@@ -0,0 +1,48 @@
+package entity
+
+import (
+	"dapp-moderator/external/token_explorer"
+	"dapp-moderator/utils"
+	"encoding/json"
+	"strings"
+)
+
+type Token struct {
+	BaseEntity      `bson:",inline"`
+	Address         string `json:"address" bson:"address"`
+	Symbol          string `json:"symbol" bson:"symbol"`
+	Slug            string `json:"slug" bson:"slug"`
+	Decimal         int    `json:"decimal" bson:"decimal"`
+	Name            string `json:"name" bson:"name"`
+	TotalSupply     string `json:"total_supply" bson:"total_supply"`
+	Owner           string `json:"owner" bson:"owner"`
+	DeployedAtBlock int    `json:"deployed_at_block" bson:"deployed_at_block"`
+}
+
+type TokenFilter struct {
+	BaseFilters
+	Name      string
+	Symbol    string
+	CreatedBy string
+	Decimal   int
+}
+
+func (t *Token) CollectionName() string {
+	return utils.COLLECTION_TOKENS
+}
+
+func (t *Token) FromTokenExplorer(te token_explorer.Token) error {
+	data, err := json.Marshal(te)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(data, t)
+	if err != nil {
+		return err
+	}
+
+	t.Slug = strings.ToLower(t.Symbol)
+
+	return err
+}
--- internal/usecase/token_explorer.go
+++ internal/usecase/token_explorer.go
@@ -3,7 +3,10 @@ package usecase
 import (
 	"context"
 	"dapp-moderator/internal/delivery/http/request"
+	"dapp-moderator/internal/entity"
 	"dapp-moderator/utils/logger"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
 
@@ -39,4 +42,56 @@ func (c *Usecase) Token(ctx context.Context, address string) (interface{}, error
 
 	logger.AtLog.Logger.Info("Token", zap.Any("data", data))
 	return data, nil
+}
+
+func (c *Usecase) CrawToken(ctx context.Context, fromPage int) (int, error) {
+	perPage := 100
+	toPage := fromPage
+
+	tokenCount := 1
+	for tokenCount > 0 {
+		params := request.PaginationReq{
+			Page:  &toPage,
+			Limit: &perPage,
+		}.ToNFTServiceUrlQuery()
+		Tokens, err := c.TokenExplorer.Tokens(params)
+		if err != nil {
+			logger.AtLog.Logger.Error("Tokens() failed", zap.Error(err))
+			return toPage, err
+		}
+		tokenCount = len(Tokens)
+		if tokenCount == 0 {
+			return toPage, nil
+		}
+
+		for _, t := range Tokens {
+			// parse token
+			token := entity.Token{}
+			if err = token.FromTokenExplorer(t); err != nil {
+				logger.AtLog.Logger.Error("FromTokenExplorer() failed", zap.Error(err))
+				return toPage, nil
+			}
+
+			// check if token exist
+			dbToken := entity.Token{}
+			err = c.Repo.FindOne(token.CollectionName(), bson.D{{"address", token.Address}}, &dbToken)
+			if err != nil && err != mongo.ErrNoDocuments {
+				logger.AtLog.Logger.Error("Find mongo entity failed", zap.Error(err))
+				return toPage, nil
+			}
+
+			if dbToken.Address != "" {
+				continue
+			}
+
+			// save token to DB
+			_, err = c.Repo.InsertOne(&token)
+			if err != nil {
+				logger.AtLog.Logger.Error("Insert mongo entity failed", zap.Error(err))
+				return toPage, nil
+			}
+		}
+		toPage++
+	}
+	return toPage, nil
 }
