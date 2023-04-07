539e48daf5eaaf0d18ea694570ee8105805087be
tri luong <tri@autonomous.nyc>
2023-04-07 18:32:28 +0700

    Fix bug get tokens

--- internal/entity/nfts.go
+++ internal/entity/nfts.go
@@ -5,22 +5,23 @@ import (
 )
 
 type Nfts struct {
-	BaseEntity      `bson:",inline"`
-	
+	BaseEntity `bson:",inline"`
+
 	Slug            string `bson:"slug" json:"slug"`
 	ContractType    string `bson:"contract_type" json:"contract_type"`
 	Contract        string `bson:"contract" json:"contract"`
 	Creator         string `bson:"creator" json:"creator"`
 	TotalItems      int    `bson:"total_items" json:"total_items"`
 	TotalOwners     int    `bson:"total_owners" json:"total_owners"`
+	Index           int64  `bson:"index" json:"index"` //autoinscreament 
 	DeployedAtBlock int64  `bson:"deployed_at_block" json:"deployed_at_block"`
 
 	//TODO - Updateable
-	Cover           string `bson:"cover" json:"cover"`
-	Thumbnail       string `bson:"thumbnail" json:"thumbnail"`
-	Name            string `bson:"name" json:"name"`
-	Description     string `bson:"description" json:"description"`
-	Social          Social `json:"social" bson:"social"`
+	Cover       string `bson:"cover" json:"cover"`
+	Thumbnail   string `bson:"thumbnail" json:"thumbnail"`
+	Name        string `bson:"name" json:"name"`
+	Description string `bson:"description" json:"description"`
+	Social      Social `json:"social" bson:"social"`
 }
 
 type FilterFiles struct {
