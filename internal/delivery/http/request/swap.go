be22ad3a5de5f1d6e6e91f32aa4f0adc92d97821
Genesis Doge <doge666@generative.xyz>
2023-05-08 10:50:14 +0700

    add ido api

---
+++ internal/delivery/http/request/swap.go
@@ -0,0 +1,16 @@
+package request
+
+import "time"
+
+type IdoRequest struct {
+	ID                string    `json:"id"`
+	TokenName         string    `json:"token_name"`
+	UserWalletAddress string    `json:"user_wallet_address"`
+	StartAt           time.Time `json:"start_at"`
+	Price             string    `json:"price"`
+	Link              string    `json:"link"`
+	Website           string    `json:"website"`
+	Twitter           string    `json:"twitter"`
+	WhitePaper        string    `json:"white_papper"`
+	Discord           string    `json:"discord"`
+}
