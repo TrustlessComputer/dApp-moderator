fc89c1a4998dd23dff94e12414d142ad3e277fa9
Genesis Doge <doge666@generative.xyz>
2023-05-09 18:37:38 +0700

    update token obj

--- internal/usecase/swap.go
+++ internal/usecase/swap.go
@@ -528,7 +528,8 @@ func (u *Usecase) TcSwapUpdateTotalSupplyJob(ctx context.Context) error {
 	for _, item := range listTokens {
 		totalSupply, _ := u.BlockChainApi.Erc20TotalSupply(item.Address)
 		if totalSupply != nil {
-			item.TotalSupply = helpers.ConvertWeiToBigFloat(totalSupply, 18).String()
+			// item.TotalSupply = helpers.ConvertWeiToBigFloat(totalSupply, 18).String()
+			item.TotalSupply = totalSupply.String()
 		}
 		err = u.Repo.UpdateToken(ctx, item)
 		if err != nil {
