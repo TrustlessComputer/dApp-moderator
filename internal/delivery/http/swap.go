87f30bd147cc7c5b33daaf97f1ddf13dcb4c6446
tungtruong <tung@myconstant.com>
2023-05-09 10:40:55 +0700

    update api token swap

--- internal/delivery/http/swap.go
+++ internal/delivery/http/swap.go
@@ -231,3 +231,17 @@ func (h *httpDelivery) getSlackReport(w http.ResponseWriter, r *http.Request) {
 		},
 	).ServeHTTP(w, r)
 }
+
+func (h *httpDelivery) jobUpdateDataSwapPair(w http.ResponseWriter, r *http.Request) {
+	response.NewRESTHandlerTemplate(
+		func(ctx context.Context, r *http.Request, vars map[string]string) (interface{}, error) {
+			err := h.Usecase.UpdateDataSwapPair(ctx)
+			if err != nil {
+				logger.AtLog.Logger.Error("jobUpdateDataSwapPair", zap.Error(err))
+				return false, err
+			}
+
+			return true, nil
+		},
+	).ServeHTTP(w, r)
+}
