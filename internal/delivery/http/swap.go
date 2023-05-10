6defa5d0ef84e48c027fb5209fa1b34665c3b4ee
Genesis Doge <doge666@generative.xyz>
2023-05-10 11:43:34 +0700

    update token report

--- internal/delivery/http/swap.go
+++ internal/delivery/http/swap.go
@@ -17,6 +17,20 @@ import (
 	req "dapp-moderator/utils/request"
 )
 
+func (h *httpDelivery) swapTmTokenScanEvents(w http.ResponseWriter, r *http.Request) {
+	response.NewRESTHandlerTemplate(
+		func(ctx context.Context, r *http.Request, vars map[string]string) (interface{}, error) {
+			err := h.Usecase.TcSwaTmTokenpScanEvents(ctx)
+			if err != nil {
+				logger.AtLog.Logger.Error("Tokens", zap.Error(err))
+				return false, err
+			}
+
+			return true, nil
+		},
+	).ServeHTTP(w, r)
+}
+
 func (h *httpDelivery) swapScanEvents(w http.ResponseWriter, r *http.Request) {
 	response.NewRESTHandlerTemplate(
 		func(ctx context.Context, r *http.Request, vars map[string]string) (interface{}, error) {
