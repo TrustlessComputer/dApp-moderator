bdddc956b27df6b5afbbadbe93c5eaf6cdb43e2e
Genesis Doge <doge666@generative.xyz>
2023-05-22 16:22:02 +0700

    Add status

--- internal/entity/token.go
+++ internal/entity/token.go
@@ -24,6 +24,7 @@ type Token struct {
 	Slug            string `json:"slug" bson:"slug"`
 
 	// edit able
+	Status          string `json:"symbol" bson:"symbol"`
 	Symbol          string `json:"symbol" bson:"symbol"`
 	Name            string `json:"name" bson:"name"`
 	Thumbnail       string `json:"thumbnail" bson:"thumbnail"`
