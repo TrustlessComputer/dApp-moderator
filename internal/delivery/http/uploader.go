5edda470d3519af09324a94e0c3c490a2e04c8ca
axe31803 <axe31803@generative.xyz>
2023-05-29 10:44:47 +0700

    Update code

--- internal/delivery/http/uploader.go
+++ internal/delivery/http/uploader.go
@@ -50,6 +50,35 @@ func (h *httpDelivery) uploadFile(w http.ResponseWriter, r *http.Request) {
 }
 
 // uploadFile godoc
+// @Summary Upload File multipart fake
+// @Description  Upload File multipart fake
+// @Tags Uploader
+// @Accept  json
+// @Produce  json
+// @Param file formData file true "file"
+// @Success 200 {object} response.UploadResponse{}
+// @Security ApiKeyAuth
+// @Router /upload/file/multipart-fake [POST]
+func (h *httpDelivery) uploadFileMultiPartFake(w http.ResponseWriter, r *http.Request) {
+	response.NewRESTHandlerTemplate(
+		func(ctx context.Context, r *http.Request, vars map[string]string) (interface{}, error) {
+
+			_, fileHeader, err := r.FormFile("file")
+			if err != nil || fileHeader == nil {
+				return nil, fmt.Errorf("invalid file data")
+			}
+
+			path, err := h.Usecase.UploadFileMultipart(fileHeader)
+			if err != nil {
+				return nil, err
+			}
+
+			return path, nil
+		},
+	).ServeHTTP(w, r)
+}
+
+// uploadFile godoc
 // @Summary Get chunks of the uploaded file
 // @Description Get chunks of the uploaded file
 // @Tags Uploader
