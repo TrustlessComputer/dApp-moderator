1bc7a0fb0b460bf6e9f3b505086038ac4460e1a5
Genesis Doge <doge666@generative.xyz>
2023-06-12 17:07:24 +0700

    update pfp in bns response

--- internal/entity/bns.go
+++ internal/entity/bns.go
@@ -20,14 +20,15 @@ type BnsPfpData struct {
 
 type FilteredBNS struct {
 	//BaseEntity        `bson:",inline"`
-	ID                string `json:"id" bson:"id"`
-	TokenID           string `json:"token_id" bson:"token_id"`
-	TokenIDInt        int64  `json:"token_id_int" bson:"token_id_int"` //use it for sort
-	Name              string `json:"name" bson:"name"`
-	Owner             string `json:"owner" bson:"owner"`
-	CollectionAddress string `json:"collection_address" bson:"collection_address"`
-	Resolver          string `json:"resolver" bson:"resolver"`
-	Pfp               string `json:"pfp" bson:"pfp"`
+	ID                string      `json:"id" bson:"id"`
+	TokenID           string      `json:"token_id" bson:"token_id"`
+	TokenIDInt        int64       `json:"token_id_int" bson:"token_id_int"` //use it for sort
+	Name              string      `json:"name" bson:"name"`
+	Owner             string      `json:"owner" bson:"owner"`
+	CollectionAddress string      `json:"collection_address" bson:"collection_address"`
+	Resolver          string      `json:"resolver" bson:"resolver"`
+	Pfp               string      `json:"pfp" bson:"pfp"`
+	PfpData           *BnsPfpData `json:"pfp_data,omitempty" bson:"pfp_data"`
 }
 
 type FilterBns struct {
