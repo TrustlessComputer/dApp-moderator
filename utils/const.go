29da5b9dee9095e514a65ab36231b33cc5c1cf08
axe31803 <axe31803@generative.xyz>
2023-06-15 11:17:59 +0700

    API - nft owners

--- utils/const.go
+++ utils/const.go
@@ -185,6 +185,7 @@ const (
 	VIEW_NEW_MARKETPLACE_NFTS                      string = "marketplace_new_nfts_view"
 	VIEW_NFT_AUCTION_AVAILABLE                     string = "nft_auction_available_view"
 	VIEW_MARKETPLACE_COLLECTION_CHART              string = "marketplace_collection_chart"
+	VIEW_MARKETPLACE_COUNT_COLLECTION_OWNER        string = "marketplace_count_collection_owner"
 	COLLECTION_MARKETPLACE_AGGREGATED_COLLECTIONS  string = "marketplace_aggregated_collections"
 
 	TRANSACTIONS string = "transactions"
