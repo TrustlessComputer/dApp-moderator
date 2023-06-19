aacb01fba825f796c3781b60e0ae0593be5dafdf
Genesis Doge <doge666@generative.xyz>
2023-06-19 09:55:11 +0700

    wip

--- internal/delivery/http/auction.go
+++ internal/delivery/http/auction.go
@@ -9,6 +9,8 @@ import (
 func (h *httpDelivery) auctionDetail(w http.ResponseWriter, r *http.Request) {
 	response.NewRESTHandlerTemplate(
 		func(ctx context.Context, r *http.Request, vars map[string]string) (interface{}, error) {
+			r.URL.Query().Get("auction_id")
+			r.Context().Value()
 			return nil, nil
 		},
 	).ServeHTTP(w, r)
