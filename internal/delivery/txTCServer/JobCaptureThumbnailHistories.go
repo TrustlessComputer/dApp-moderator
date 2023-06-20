5801f0ba0a9bfb5014f3fcd325c5fd0b9630cc02
axe31803 <axe31803@generative.xyz>
2023-06-20 15:44:40 +0700

    Crontab - image's size

--- internal/delivery/txTCServer/JobCaptureThumbnailHistories.go
+++ internal/delivery/txTCServer/JobCaptureThumbnailHistories.go
@@ -1,8 +1,10 @@
 package txTCServer
 
 import (
+	"context"
 	"dapp-moderator/internal/usecase"
 	"dapp-moderator/utils/global"
+	"sync"
 	"time"
 )
 
@@ -18,8 +20,21 @@ func NewJobCaptureThumbnailHistories(global *global.Global, uc usecase.Usecase) 
 }
 
 func (c *JobCaptureThumbnailHistories) StartServer() {
+	var wg sync.WaitGroup
 	for {
-		c.UseCase.SoulNftImageHistoriesCrontab()
+
+		wg.Add(2)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			c.UseCase.SoulNftImageHistoriesCrontab()
+		}(&wg)
+
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			c.UseCase.CrontabUpdateImageSize(context.Background())
+		}(&wg)
+
+		wg.Wait()
 		time.Sleep(time.Minute * 20) //20 min
 	}
 }
