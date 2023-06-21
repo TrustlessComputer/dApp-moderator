c3f519a5e6f43dc0a75c11fd2aa827c5121bf2f1
Genesis Doge <doge666@generative.xyz>
2023-06-21 14:18:14 +0700

    update response

--- internal/delivery/http/response/auction.go
+++ internal/delivery/http/response/auction.go
@@ -7,6 +7,9 @@ type AuctionDetailResponse struct {
 	AuctionStatus entity.AuctionStatus `json:"auction_status"`
 	HighestBid    string               `json:"highest_bid"`
 	EndTime       string               `json:"end_time"`
+
+	DBAuctionID    string `json:"db_auction_id"`
+	ChainAuctionID string `json:"chain_auction_id"`
 }
 
 type AuctionBidResponseItem struct {
