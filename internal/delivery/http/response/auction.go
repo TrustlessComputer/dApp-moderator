e284ea6bbc733ffd283a31ee057d0fc1360568e1
vinkent420 <135956574+vinkent420@users.noreply.github.com>
2023-06-30 13:45:19 +0700

    Ft/420 (#70)
    * add response to auction detail
    
    * revert changes wrong

--- internal/delivery/http/response/auction.go
+++ internal/delivery/http/response/auction.go
@@ -7,10 +7,13 @@ import (
 )
 
 type AuctionDetailResponse struct {
-	Available     bool                 `json:"available"`
-	AuctionStatus entity.AuctionStatus `json:"auction_status"`
-	HighestBid    string               `json:"highest_bid"`
-	EndTime       string               `json:"end_time"`
+	Available           bool                 `json:"available"`
+	AuctionStatus       entity.AuctionStatus `json:"auction_status"`
+	HighestBid          string               `json:"highest_bid"`
+	HighestBidder       string               `json:"highest_bidder"`
+	HighestBidderAvatar string               `json:"highest_bidder_avatar,omitempty"`
+	HighestBidderName   string               `json:"highest_bidder_name,omitempty"`
+	EndTime             string               `json:"end_time"`
 
 	DBAuctionID    string `json:"db_auction_id"`
 	ChainAuctionID string `json:"chain_auction_id"`
